8344eb9e5a768c8ccb4fa2f061b33aabc86e259e
adrian <adrianjost1@gmx.de>
2023-01-27 14:46:29 +0100

    mp main menu additions and tweaks

--- CapybaraStudios/Assets/Scripts/MultiPlayer/Lobby/LobbyManager.cs
+++ CapybaraStudios/Assets/Scripts/MultiPlayer/Lobby/LobbyManager.cs
@@ -11,9 +11,18 @@ public class LobbyManager : MonoBehaviour
 {
     [SerializeField] private TMP_InputField lobbyName;
     [SerializeField] private TMP_InputField maxPlayers;
+
+    [SerializeField] private GameObject lobbyList;
+
     // Start is called before the first frame update
     private Lobby lobby;
     private string playerId;
+
+
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    private List<GameObject> entries;
+
     async void Start()
     {
         await UnityServices.InitializeAsync();
@@ -22,80 +31,133 @@ public class LobbyManager : MonoBehaviour
         playerId = AuthenticationService.Instance.PlayerId;
     }
 
-    public void ValidateLobbyName(string text) {
-        if(text == "") {
+    public void Awake()
+    {
+        entryContainer = lobbyList.transform.Find("EntryContainer");
+        entryTemplate = entryContainer.Find("EntryTemplate");
+        entries = new List<GameObject>();
+        entryTemplate.gameObject.SetActive(false);
+    }
+
+    public void ValidateLobbyName(string text)
+    {
+        if (text == "")
+        {
             lobbyName.text = "My Lobby";
         }
     }
 
-    public void ValidateMaxPlayers(string text) {
-        if(text == "") {
+    public void ValidateMaxPlayers(string text)
+    {
+        if (text == "")
+        {
             maxPlayers.text = "2";
             return;
         }
+
         int num = int.Parse(text);
-        if(num > 10) {
+        if (num > 10)
+        {
             maxPlayers.text = "10";
-        } else if(num == 0) {
+        }
+        else if (num == 0)
+        {
             maxPlayers.text = "2";
         }
     }
 
-    public async void CreateLobby() {
-        try {
+    public async void CreateLobby()
+    {
+        try
+        {
             lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName.text, int.Parse(maxPlayers.text));
             StartCoroutine(UpdateLobbyTimeoutCoroutine(15));
             //hier muss auch map etc rein
             Debug.Log("Created Lobby " + lobby.Name + " " + lobby.MaxPlayers);
-        } catch(LobbyServiceException e) {
+        }
+        catch (LobbyServiceException e)
+        {
             Debug.Log(e);
         }
     }
 
-    private IEnumerator UpdateLobbyTimeoutCoroutine(float secs) {
+    private IEnumerator UpdateLobbyTimeoutCoroutine(float secs)
+    {
         var delay = new WaitForSecondsRealtime(secs);
-        while(true) {
+        while (true)
+        {
             Lobbies.Instance.SendHeartbeatPingAsync(lobby.Id);
             yield return delay;
         }
     }
 
-    public async void ListLobbies() {
-        try {
-            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions {
+    public async void ListLobbies()
+    {
+        try
+        {
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions
+            {
                 Count = 5,
-                Filters = new List<QueryFilter> {
+                Filters = new List<QueryFilter>
+                {
                     new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
                 },
-                Order = new List<QueryOrder> {
+                Order = new List<QueryOrder>
+                {
                     new QueryOrder(false, QueryOrder.FieldOptions.Created)
                 }
             };
             QueryResponse response = await Lobbies.Instance.QueryLobbiesAsync(queryLobbiesOptions);
-            foreach(Lobby lobby in response.Results) {
+
+            var templateHeight = 40f;
+            var index = 0;
+            foreach (Lobby lobby in response.Results)
+            {
                 Debug.Log(lobby.Id + " " + lobby.Name + " " + lobby.MaxPlayers);
+
+
+                var entryTransform = Instantiate(entryTemplate, entryContainer);
+                entries.Add(entryTransform.gameObject);
+                var entryRectTransform = entryTransform.GetComponent<RectTransform>();
+                entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * index);
+                entryTransform.gameObject.SetActive(true);
+
+                entryTransform.Find("Name").GetComponent<TMP_Text>().text = lobby.Name;
+                entryTransform.Find("PlayerNum").GetComponent<TMP_Text>().text = lobby.AvailableSlots+"/"+lobby.MaxPlayers;
+                //entryTransform.Find("JoinBtn") = ;
+                
+                index++;
             }
-        } catch(LobbyServiceException e) {
+        }
+        catch (LobbyServiceException e)
+        {
             Debug.Log(e);
-        }   
+        }
     }
+
 
     /*public async JoinLobby() {
         await Lobbies.Instance.JoinLobbyByIdAsync();
     }*/
 
-    private void OnDestroy() {
+    private void OnDestroy()
+    {
         LeaveLobby();
     }
 
-    public void LeaveLobby() {
-        try {
+    public void LeaveLobby()
+    {
+        try
+        {
             StopAllCoroutines();
-            if(lobby != null) {
-                if(lobby.HostId == playerId) Lobbies.Instance.DeleteLobbyAsync(lobby.Id);
+            if (lobby != null)
+            {
+                if (lobby.HostId == playerId) Lobbies.Instance.DeleteLobbyAsync(lobby.Id);
                 else Lobbies.Instance.RemovePlayerAsync(lobby.Id, playerId);
             }
-        } catch(LobbyServiceException e) {
+        }
+        catch (LobbyServiceException e)
+        {
             Debug.Log(e);
         }
     }
